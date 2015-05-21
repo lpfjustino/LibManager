dd3bc544369c3319fc82a5534f6a16df368f45dc
Lpfjustino <Lpfjustino@LPFJ>
2015-05-21 15:00:39 -0300

    Trying to create branch

--- src/libmanager/LibManager.java
+++ src/libmanager/LibManager.java
@@ -6,10 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Lpfjustino
- */
 public class LibManager extends Application {
     
     @Override

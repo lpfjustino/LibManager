e9341ca8f77f32af66b64b863143f5c92492b6fa
Lpfjustino <Lpfjustino@LPFJ>
2015-05-27 20:05:56 -0300

    Fixing ZeMane's stupid importations and stuff.

--- src/libmanager/HomeScreenController.java
+++ src/libmanager/HomeScreenController.java
@@ -1,37 +1,39 @@
 package libmanager;
 
-import java.awt.Button;
+import static com.sun.glass.ui.android.SoftwareKeyboard.show;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class HomeScreenController implements Initializable {
-	
+    Stage primaryStage;
+    
     @FXML
-    private Button button;
+    private Button newUserButton;
     
     @FXML
     private void newUserButtonAction(ActionEvent event) {
-    	
+    	primaryStage = (Stage) ((Button) event.getSource()).getScene().getWindow();
     	Platform.runLater(
-    			() -> {
-    				try {
-    					Parent newUser = FXMLLoader.load(getClass().getResource("NewUserScreen.fxml"));
-    					Scene scene = new Scene(newUser);
-    					libManager.programStage.setScene(scene);
-    					libManager.programStage.show();
-    				} catch (IOException e) { }
-
-    			} );
+            () -> {
+                try {
+                    Parent newUser = FXMLLoader.load(getClass().getResource("/screens/NewUserScreen.fxml"));
+                    Scene scene = new Scene(newUser);
+                    primaryStage.setScene(scene);
+                    primaryStage.show();
+                } catch (IOException ex) { }
+            });
     }
     
     @FXML
